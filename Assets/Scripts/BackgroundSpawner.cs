8f51111fe6b3b0acd5952a261da2d8aeec389226
Leon <vilovic.leon@gmail.com>
2026-03-14 16:05:31 +0100

    ObjectPoolerBackgroundObjects now spawns with chance spread

--- Assets/Scripts/BackgroundSpawner.cs
+++ Assets/Scripts/BackgroundSpawner.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class BackgroundSpawner : MonoBehaviour
 {
     [SerializeField] private ObjectPoolerBackgroundObjects objectPoolerBackgroundObjects;
+    [SerializeField] private float spawnRangeDown = -2.7f;
+    [SerializeField] private float spawnRangeUp = 2.7f;
     [SerializeField] private float spawnRateRangeDown = 0f;
     [SerializeField] private float spawnRateRangeUp = 0.05f;
     [SerializeField, Range(0f, 1.5f)] private float objectMoveSpeedEasingFactor = 0.4f;
     [SerializeField] private float objectMoveSpeedFactor = 1f;
     [SerializeField] private float objectMoveSpeedRandomFactor = 0.1f;
- 
 
     private float randomIntFrequency = 0.5f;
 
@@ -30,13 +31,9 @@ public class BackgroundSpawner : MonoBehaviour
             randomIntFrequency = Random.Range(spawnRateRangeDown, spawnRateRangeUp);
             
             GameObject obj =  objectPoolerBackgroundObjects.GetPooledObject();
-            float yPosition = Random.Range(-2.5f, 2.5f);
+            float yPosition = Random.Range(spawnRangeDown, spawnRangeUp);
             obj.transform.position = transform.position + new Vector3(0, yPosition, 0);
             BacgroundObject bacgroundObject = obj.GetComponent<BacgroundObject>();
-
-            //float y = Mathf.Abs(yPosition);
-            //float eased = y * y * (3f - 2f * y);
-            //float eased = y * (3f - 2f * y);
 
             bacgroundObject.speed = Mathf.Abs(yPosition) * objectMoveSpeedEasingFactor + Random.Range(objectMoveSpeedFactor, objectMoveSpeedFactor + objectMoveSpeedRandomFactor); 
             obj.SetActive(true);
--- Assets/Scripts/ObjectPoolerBackgroundObjects.cs
+++ Assets/Scripts/ObjectPoolerBackgroundObjects.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 public class ObjectPoolerBackgroundObjects : MonoBehaviour
 {
-    public List<GameObject> prefabList;
     public List<ChancedPrefab> chancedPrefabList;
     public int poolSize = 30;
+    private List<GameObject> pool;
+    private List<float> chanceSpread;
 
     [System.Serializable]
     public class ChancedPrefab
@@ -13,11 +16,10 @@ public class ObjectPoolerBackgroundObjects : MonoBehaviour
         public GameObject prefab;
         public float chance;
     }
-
-    private List<GameObject> pool;
     void Start()
     {
         CreatePool();
+
     }
     private void CreatePool()
     {
@@ -27,21 +29,54 @@ public class ObjectPoolerBackgroundObjects : MonoBehaviour
             CreateNewObject();
         }
     }
+    static int FindLargestSmallerOrEqual(List<float> list, float target)
+    {
+        int left = 0;
+        int right = list.Count - 1;
+        int result = -1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (list[mid] == target)
+            {
+                return mid;
+            }
+            else if (list[mid] < target)
+            {
+                result = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
     private List<float> CreateChanceSpreadList()
     {
         List<float> list = new List<float>();
-        float sum = 0f;
+        float ChanceSum = 0;
         list.Add(0f);
         foreach (ChancedPrefab chancedPrefab in chancedPrefabList)
         {
-            sum += chancedPrefab.chance;
-            list.Add(sum);
+            ChanceSum += chancedPrefab.chance;
+            list.Add(ChanceSum);
         }
         return list;
     }
     private GameObject CreateNewObject()
     {
-        int index = Random.Range(0, chancedPrefabList.Count);
+        if (chanceSpread == null || chanceSpread.Count == 0)
+        {
+            chanceSpread = CreateChanceSpreadList();
+        }
+
+        float random = Random.Range(0f, chanceSpread[chanceSpread.Count-1]);
+        int index = FindLargestSmallerOrEqual(chanceSpread, random);
+        //Debug.Log("Random:" + random + " Index:" + index);
         GameObject obj = Instantiate(chancedPrefabList[index].prefab, transform.position, transform.rotation);
         obj.SetActive(false);
         pool.Add(obj);
